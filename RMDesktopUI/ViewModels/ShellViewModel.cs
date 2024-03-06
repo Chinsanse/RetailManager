76cad62fb838b963c43bf12d74f3bb2a8423d95e
Chinsanse <brandon.chin76@gmail.com>
2024-03-06 17:29:06 +0100

    Add Users and Groups/Roles Display
    Get list of users and roles from Entity Framework and display it in an administrative view.

--- RMDesktopUI/ViewModels/ShellViewModel.cs
+++ RMDesktopUI/ViewModels/ShellViewModel.cs
@@ -51,6 +51,11 @@ namespace RMDesktopUI.ViewModels
             TryClose();
         }
 
+        public void UserManagement()
+        {
+            ActivateItem(IoC.Get<UserDisplayViewModel>());
+        }
+
         public void LogOut()
         {
             _user.ResetUserModel();
