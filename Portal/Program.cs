16e17569776304ae056fa7d5615e85a5c1964735
Chinsanse <brandon.chin76@gmail.com>
2024-04-30 13:24:14 +0200

    Add Registration System
    Updates API to allow users to register and add a Blazor Form for accepting registration infos.

--- Portal/Program.cs
+++ Portal/Program.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Portal;
 using Portal.Authentification;
+using RMDesktopUI.Library.Api;
+using RMDesktopUI.Library.Models;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -13,6 +15,14 @@ builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
+
+builder.Services.AddSingleton<IAPIHelper, APIHelper>();
+builder.Services.AddSingleton<ILoggedInUserModel, LoggedInUserModel>();
+builder.Services.AddTransient<IProductEndpoint, ProductEndpoint>();
+builder.Services.AddTransient<IUserEndpoint, UserEndpoint>();
+builder.Services.AddTransient<ISaleEndpoint, SaleEndpoint>();
+
+
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
---
+++ RMDesktopUI.Library/Models/CreateUserModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RMDesktopUI.Library.Models
+{
+    public class CreateUserModel
+    {
+        [Required]
+        [DisplayName("First Name")]
+        public  string FirstName { get; set; }
+
+        [Required]
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [DisplayName("Email Address")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [DisplayName("Confirm Password")]
+        [Compare(nameof(Password), ErrorMessageResourceName ="The passwords do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
